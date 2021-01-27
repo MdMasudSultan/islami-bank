a2b14656f210f4a2478da641f2306dd9c1cd79ea
Md Masud Sultan <mdmasudsultan910@gmail.com>
2021-01-27 18:26:02 +0600

    this my bank

--- bank.js
+++ bank.js
@@ -1,3 +1,6 @@
+
+// login button event handlar
+
 let btnclick = document.getElementById('login')
 btnclick.addEventListener("click", function(){
     let fromArea = document.getElementById('bank-from')
@@ -6,3 +9,43 @@ btnclick.addEventListener("click", function(){
     tranArea.style.display = 'block'
 
 })
+
+// deposit button event hanlar
+
+const depositBtn = document.getElementById('addDeposit');
+depositBtn.addEventListener('click', function(){
+    const depositNumber = getInputNumber('depositAmount')
+
+    updateSpanText("currentDeposit",depositNumber)
+    
+    updateSpanText("currentBalance", depositNumber)
+
+    document.getElementById('depositAmount').value = "";
+
+    // withdrow button event handlar
+
+    const withdraw = document.getElementById('addWithdraw');
+    withdraw.addEventListener('click', function() {
+        const withdrawAmount = getInputNumber('withdrawAmount');
+
+        updateSpanText('currentWithdraw',withdrawAmount);
+        updateSpanText('currentBalance',-1*withdrawAmount);
+
+        document.getElementById('withdrawAmount').value = "";
+       
+      
+    })
+// addEventListener fun
+    function getInputNumber(id) {
+        const amount = document.getElementById(id).value;
+        const amountNumber = parseFloat(amount);
+        return amountNumber
+      
+    }
+})
+function updateSpanText(id,depositNumber) {
+    const current = document.getElementById(id).innerText;
+    const currentNumber = parseFloat(current);
+    const totalAmount = currentNumber + depositNumber;
+    document.getElementById(id).innerText = totalAmount;
+}
